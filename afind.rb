83cb57670c075f7f54ef71bc722c7f920589197f
Bin Chen <bin.chen@linaro.org>
2016-06-03 21:00:50 +1000

    dexopt aware
    $ afind Intent.java
    Android.mk  : /framework/base/Android.mk
    Jar         : /system/framework/arm64/boot-framework.oat/.art
    
    $ afind CameraService.java
    Android.mk  : /framework/base/services/Android.mk
    Jar         : /system/framework/oat/arm64/services.odex (dexopt is on)
    
    $ afind TvRemoteProvider.java
    Android.mk  : /framework/base/media/lib/tvremote/Android.mk
    Jar         : /system/framework/com.android.media.tv.remoteprovider.jar

--- afind.rb
+++ afind.rb
@@ -4,21 +4,26 @@
 
 pwd = `pwd`
 
-if pwd.chomp.end_with?("framework")
-    puts "run this program in framework directory"
+if !pwd.chomp.end_with?("frameworks")
+    puts "Error: run this program in Android frameworks directory"
     exit
 end 
 
 if ARGV.size != 1
-    puts "usage: mfind XXXX.java"
+    puts "Usage: afind XXXX.java"
     exit
 end
 
 debug = false
 targetFile = ARGV[0]
-puts "Finding where to build #{targetFile}"
 
 target = `find . -name #{targetFile}`
+
+if target.empty?
+    puts "Error: Faild to find #{targetFile}"
+    exit
+end
+
 allMk = `find ./base -name Android.mk`
 
 puts allMk if debug
@@ -41,7 +46,7 @@ possibleMk = possibleMk.select { |p|
 puts possibleMk if debug
 
 if (possibleMk.size == 0)
-    puts "failed to find the Android.mk for #{target}"
+    puts "Error: Failed to find the Android.mk for #{target}"
     exit
 end
 
@@ -55,8 +60,8 @@ r = `grep -in BUILD_JAVA_LIBRARY #{targetMk}`
 location = r.split(":")[0].to_i
 
 module_name=""
-t = `grep -in LOCAL_MODULE #{targetMk}`
-t.split("\n").select { |e|  
+t = `grep -in "LOCAL_MODULE\s*:=" #{targetMk}`
+t.split("\n").select { |e| 
     l = e.split(":")[0].to_i
     # find it
     if (l < location)
@@ -64,7 +69,32 @@ t.split("\n").select { |e|
     end
 }
 
+arch="arm64"
+artifact = "/system/framework/#{module_name}.jar"
+#There possible output
+#1. module_name.jar  in framework/
+#2. module_name.odex in framework/oat/arch
+#3. boot-module_name.art and boot-module_name.oat file in framework/arch/
+ANDROID_PRODUCT_OUT=ENV["ANDROID_PRODUCT_OUT"]
+
+if (!ANDROID_PRODUCT_OUT.empty?)
+    # check case 2
+    r = %x(find #{ANDROID_PRODUCT_OUT}/system/framework -name #{module_name}.odex)
+    if !r.empty?
+        artifact = "/system/framework/oat/#{arch}/#{module_name}.odex (dexopt is on)"
+    else
+    #check case 3
+        r = %x(find #{ANDROID_PRODUCT_OUT}/system/framework -name boot-#{module_name}.oat)
+        if !r.empty?
+            artifact = "/system/framework/#{arch}/boot-#{module_name}.oat/.art"
+        end
+    end
+else
+    puts "Waring: the output could also be .odex or .oat file. But in order to detect that you
+         need to set ANDROID_PRODUCT_OUT environment"
+end
+
 puts <<OUTPUTT
 Android.mk  : /framework/#{targetMk[2..-1]} 
-Jar         : /system/framework/#{module_name}.jar"
+Jar         : #{artifact}
 OUTPUTT
